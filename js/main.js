40bf293debcc66c5095e604a595231e42dc6f0ef
FazleRabbi <24.fazlerabbi@gmail.com.com>
2021-02-07 03:05:17 -0600

    code cleaning and put commenting

--- js/main.js
+++ js/main.js
@@ -1,60 +1,52 @@
-document.getElementById('searchBtn').addEventListener('click', function () {
+
+// click handler on search section 
+document.getElementById('searchBtn').addEventListener('click', clickEvent => {
 
     let searchValue = document.getElementById('input').value ;
-    console.log(searchValue)
     fetch('https://www.themealdb.com/api/json/v1/1/search.php?s='+searchValue)
     .then(res=>res.json())
-    .then(data=>getInfo(data.meals))
-    console.log('ami kisui parina')
+    .then(data=>getInfo(data.meals))//getinfo fuction call by ES6
+    
 })
 
+////function declaration
 const getInfo = name =>{
-    
-if (name === null) {
-    
-    const div = document.createElement('div');
-        const foodCarts = `
-                <h3 class="text-info"> I will kill you if you search this type of food again. Thank you!!</h3>
-        `
-        div.innerHTML = foodCarts;
-       
-        const mainDiv = document.getElementById('main');
-        console.log(mainDiv);
-        mainDiv.appendChild(div);
-
-    console.log('ajaira jinis search korlei ki pawa jay?')
-
-}else{
-    name.forEach(food => {
-        console.log(food)
-        const foodName = food.strMeal;
-        const imgUrl = food.strMealThumb;
-        const foodId = food.idMeal;
+    if (name === null) {
         
         const div = document.createElement('div');
-        const foodCarts = `
-            <div onclick ="getDetails(${foodId},'${imgUrl}','${foodName}')" id='foodCart' class = 'foodCart'>
-                <img src="${imgUrl}" alt="">
-                <h3>${foodName}</h3>
-            </div>
-        `
-        div.innerHTML = foodCarts;
-        div.className = 'foodCarts';
-       
-        const mainDiv = document.getElementById('main');
-        console.log(mainDiv);
-        mainDiv.appendChild(div);
-
-    });
+            const foodCarts = `
+                    <h3 class="text-info"> I will kill you if you search this type of food again. Thank you!!</h3>
+            `
+            div.innerHTML = foodCarts;
+            const mainDiv = document.getElementById('main');
+            mainDiv.appendChild(div);
+    }else{
+        ////adding for each for array element
+        name.forEach(food => {
+            const foodName = food.strMeal;
+            const imgUrl = food.strMealThumb;
+            const foodId = food.idMeal;
+            const div = document.createElement('div');
+            const foodCarts = `
+                <div onclick ="getDetails(${foodId},'${imgUrl}','${foodName}')" id='foodCart' class = 'foodCart'>
+                    <img src="${imgUrl}" alt="">
+                    <h3>${foodName}</h3>
+                </div>
+            `
+            div.innerHTML = foodCarts;
+            div.className = 'foodCarts';
+            const mainDiv = document.getElementById('main');
+            mainDiv.appendChild(div);
+        });
 }
- 
 }
 
+////declaring getDetails funtion for food ingredient
 const getDetails =(foodId , imgUrl, foodName) =>{
     fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i='+foodId)
     .then(res=>res.json())
     .then(data=>{
-    
+            //storing food ingredient to variable
             const ingredient1= data.meals[0].strIngredient1;
             const ingredient2= data.meals[0].strIngredient2;
             const ingredient3= data.meals[0].strIngredient3;
@@ -64,14 +56,11 @@ const getDetails =(foodId , imgUrl, foodName) =>{
             const ingredient7= data.meals[0].strIngredient7;
             const ingredient8= data.meals[0].strIngredient8;
             const ingredient9= data.meals[0].strIngredient9;
-
-
             const li = `
             <div class = 'foodCart half-width'>
                 <img src="${imgUrl}" alt="">
                 <h3>${foodName}</h3>
             </div>
-            
             <div class = 'half-width'>
                 <h1>Meal Ingredient</h1>
                 <ul>
@@ -87,27 +76,13 @@ const getDetails =(foodId , imgUrl, foodName) =>{
                 </ul>
             </div>
             `
-
-
-            // const li = `
-            //     <li>${ingredient1}</li>
-            //     <li>${ingredient2}</li>
-            //     <li>${ingredient3}</li>
-            //     <li>${ingredient4}</li>
-            //     <li>${ingredient5}</li>
-            //     <li>${ingredient6}</li>
-            //     <li>${ingredient7}</li>
-            //     <li>${ingredient8}</li>
-            //     <li>${ingredient9}</li>
-            // `
-         
         const div = document.createElement('div');
         div.innerHTML = li;
         div.className='cartInfo';
-
         const foodDetailsDiv = document.getElementById('foodDetails');
-        console.log(foodDetailsDiv)
         foodDetailsDiv.appendChild(div); 
     
     })
 }
+
+//thank you!!
